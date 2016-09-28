0440ed17a811e6eaec0d259bd9aaeea7506013f7
Amanda Oh <[DAmandaGoh>
2016-09-28 21:58:56 +0800

    updated styling with functioning start, pause and reset buttons

--- Js/script.js
+++ Js/script.js
@@ -5,11 +5,13 @@ function init () {
   var body = document.querySelector('body')
   var h1 = document.querySelector('h1')
   var startButton = document.querySelector('#start')
+  var startTime = 0
 
-  startButton.addEventListener('click', startTimer)
+  startButton.addEventListener('click', updateTime)
 
-  function startTimer() {
-    window.setInterval(start, 1000)
+  function updateTime() {
+    startTime = window.setInterval(start, 1000)
+    return startTime
   }
   var time = 0
   function start () {
@@ -17,4 +19,25 @@ function init () {
     h1.textContent = "Time elapsed: " + time
   }
 
+  var pauseButton = document.querySelector('#pause')
+
+  pauseButton.addEventListener('click',pauseTime)
+
+  function pauseTime(){
+    window.clearInterval(startTime)
+  }
+
+  var resetButton = document.querySelector('#reset')
+
+  resetButton.addEventListener('click', resetTime)
+
+  function resetTime(){
+    window.clearInterval(startTime)
+    time = 0
+    h1.textContent = "Time elapsed: " + time
+    window.setTimeout(stopWatch, 1500)
+    function stopWatch (){
+      h1.textContent = "Stop Watch"
+    }
+  }
 }
